085df33731f82dd8811e5e8e17de40318bd976d9
fawasam <fawasam5@gmail.com>
2022-05-25 22:03:44 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Explore from './pages/Explore'
@@ -9,6 +8,7 @@ import SignUp from './pages/SignUp'
 import Offer from './pages/Offer'
 import ForgotPassword from './pages/ForgotPassword'
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -17,7 +17,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Explore/>}/>
         <Route path='/offers' element={<Offer/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/profile' element={<PrivateRoute/>}>
+           <Route path='/profile' element={<Profile/>}/>
+        </Route>
         <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
         <Route path='/forgot-password' element={<ForgotPassword/>}/>
